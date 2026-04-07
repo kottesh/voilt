ba3d1989cb875ae55989aefba6c26d87664d0629
anushm55 <143617212+AnushM55@users.noreply.github.com>
2026-04-07 10:12:27 +0530

    Feat/client side UI (#3)
    * init frontend dashboard scaffold
    
    * feat(ui): add vechile voilation logs in a table
    
    * chore: update ignores
    
    * server: add email alert for confirmed violations
    
    ---------
    
    Co-authored-by: VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import {VehicleTable} from "./components/table"
+function App() {
+
+  return (
+    <>
+      <VehicleTable></VehicleTable>
+    </>
+  )
+}
+
+export default App
